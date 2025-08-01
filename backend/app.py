23a44e965f2907185eb758da7e34a6fda41ded0b
VahiyaatProduct9 <kishordebnath123123@gmail.com>
2025-08-01 14:57:16 +0530

    connected both sides

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["*","http://localhost:3000", "https://stylemorph.vercel.app"], # Change this to your liking
+    allow_origins = ["http://localhost:3000", "https://stylemorph.vercel.app"], # Change this to your liking
     allow_methods = ["*"],
     allow_headers = ["*"]
 )
